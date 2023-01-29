5ea1689901a072d1d7cc1ce2a715ddb3e3c6cb0d
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 13:05:17 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,5 +1,13 @@
-let username = prompt('Holla What is your name?');
-console.log('Hello, ' + username);
+let userGreeting() {
+    userName = prompt("Hola! What's your first name?");
+    console.log(userName);
+    document.write("Hola " + userName+"! ");
+    return userName;
+
+
+
+
+
 
 
 
