6e6c23240af1da0ce91b7c6badc758f135f6a16f
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:33:20 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,3 +2,7 @@ let username = prompt('What is your name?');
 console.log('Hello, ' + username);
 
 alert("You must love Stand Up Paddle Boarding to view this website");
+
+while(age < 21) {
+    age = prompt('You must be 21 or older to enter'); 
+}
