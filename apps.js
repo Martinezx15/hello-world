d83a37b496c19812258a4f25f93c835c42c82e2d
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:39:48 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,8 +1,12 @@
 let username = prompt('What is your name?');
 console.log('Hello, ' + username);
 
-alert("You must love Stand Up Paddle Boarding to view this website");
+alert("You must love Fishing to view this website");
 
 while(age < 21) {
     age = prompt('You must be 21 or older to enter'); 
 }
+ document.write("Please click the link above which has great restaurant suggestions");
+ } 
+let time = prompt('What hour is it? (0-23)');
+let message;
