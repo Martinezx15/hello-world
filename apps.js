3c396f5d2a73871dab53ed81270eb68b568fdfe3
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 21:15:03 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -7,21 +7,12 @@ function myAlert()
 }
 alert("You must love Fishing to view this website");
 
-var userPreference;
+function fish(){
+let fishAnswer =prompt('What is your favorite fish?');
 
-function ratPicture() {
-    let number = prompt("How many ratmen can you fight? Pick a number between 1 and 3");
-    console.log(number);
-
-    if (number == 1 || number == 2 || number == 3) {
-        console.log(number);
-    } else {
-        ratPicture();
-    }
-
-    for (let i = 0; i < number; i++) {
-        document.write("<img src='./stormvermin.jpg' width = '32%'></img>");
-        console.log(number);
-        console.log(i);
-    }
+if (movieAnswer == 'cods'){
+    document.write(' You got it!   ')
+}
+else {document.write(' Nope. It is cods');
+} 
 }
