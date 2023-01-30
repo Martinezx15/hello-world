17192815b1371b7911960b2108e94ec66b9a8eac
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:21:10 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,6 +1,19 @@
 let userGreeting() {
     userName = prompt("Hola! What's your first name?");
     console.log(userName);
-    document.write("Hola " + userName+"! ");
+    document.write("Aloha " + userName+"! ");
     return userName;
 }
+
+userGreeting();
+
+alert("You must love Fishing to view this website");
+
+function loveConfirm() {
+    let love = prompt("Do you love Stand Up Paddle Boarding? yes or no.");
+
+    if (love) {
+        while (love.toLowerCase() !== "yes") {
+            loveConfirm();
+            console.log(love);
+            return love;
