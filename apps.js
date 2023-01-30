7d6a5ac3e57c98cd78c5e32880eb427f0d363cea
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:45:22 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,11 +2,3 @@ let username = prompt('What is your name?');
 console.log('Hello, ' + username);
 
 alert("You must love Fishing to view this website");
-
-while(age < 21) {
-    age = prompt('You must be 21 or older to enter'); 
-}
- document.write("Please click the link above which has great restaurant suggestions");
- } 
-let time = prompt('What hour is it? (0-23)');
-let message;
