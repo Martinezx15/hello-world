a52d95cf5bd1c1395ca0f2569b13454916edcc60
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 21:35:39 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,7 +1,5 @@
 let username = prompt("What is your name?");
 console.log("Hello, " + username);
-function myAlert() {
-  alert("I am an event handler....");
-  return;
-}
-alert("You must love Fishing to view this website");
+
+let time = prompt('What hour is it? (0-23)');
+let message;
