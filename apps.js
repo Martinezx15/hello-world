4ba09455cba4a914f78f477f3bfde6acc30cdd65
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 21:30:59 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,10 +1,7 @@
-let username = prompt('What is your name?');
-console.log('Hello, ' + username);
-function myAlert()
-{
-    alert("I am an event handler....");
-	return;
+let username = prompt("What is your name?");
+console.log("Hello, " + username);
+function myAlert() {
+  alert("I am an event handler....");
+  return;
 }
 alert("You must love Fishing to view this website");
-
-
