39082e35a831ed56fde2953fde5d14c5255e9d80
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:58:39 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,4 +1,16 @@
 let username = prompt('What is your name?');
 console.log('Hello, ' + username);
-
+function myAlert()
+{
+    alert("I am an event handler....");
+	return;
+}
 alert("You must love Fishing to view this website");
+
+var userPreference;
+
+if (confirm("Do you want to save changes?") == true) {
+    userPreference = "Data saved successfully!";
+} else {
+    userPreference = "Save Cancelled!";
+}
