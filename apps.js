22b05f98f933035e0df0343355cf3831c1636127
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 21:20:59 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -7,12 +7,20 @@ function myAlert()
 }
 alert("You must love Fishing to view this website");
 
-function fish(){
-let fishAnswer =prompt('What is your favorite fish?');
+function consoleChoice() {
+  let number = prompt(
+    "Pick a number depending on your preferred gaming console. 1 for PC, 2 for Xbox, 3 for Playstation, or 4 for none of the above";
+  );
+  console.log(number);
+  if (number == 1 || number == 2 || number == 3) {
+    console.log(number);
+  } else {
+    consoleChoice();
+  }
 
-if (movieAnswer == 'cods'){
-    document.write(' You got it!   ')
-}
-else {document.write(' Nope. It is cods');
-} 
+  for (let i = 0; i < number; i++) {
+    document.write("<img src='./surfacelaptop.jpg' width = '50%'></img>");
+    console.log(number);
+    console.log(i);
+  }
 }
