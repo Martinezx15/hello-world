f829693441a374c128ed21f61110804c638b918b
Alexander Urena Martinez <alexanderraum@gmail.com>
2023-01-29 20:31:05 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,7 +1,3 @@
- let userName = prompt("Welcome to Fishing for fun! What is your name?");
-  console.log(userName);
-  document.write("Thank you for fishing with us today " + userName);
-  return userName;
-}
-
+let username = prompt('What is your name?');
+console.log('Hello, ' + username);
 
